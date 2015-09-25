e2c1190c3ed8bba1a7085f654dc08c0f39a01205
Thorsten Lorenz <thlorenz@gmx.de>
2015-09-25 16:23:25 +0200

    adding class ids filter
    - if not supplied, all async_wrap providers are included
    - otherwise only the objects matching the class ids provided by the user
      will be included

--- persistents-with-classid.js
+++ persistents-with-classid.js
@@ -1,17 +1,23 @@
 'use strict';
 
-var binding = process._linkedBinding('persistents_with_classid');
+const binding = process._linkedBinding('persistents_with_classid');
+const async_wrap = process.binding('async_wrap');
 
-var visit = exports.visit = function visit(fn) {
-  binding.visit(fn);
+function getProviderValue(k) { return async_wrap.Providers[k] }
+const providerIds = Object.keys(async_wrap.Providers).map(getProviderValue);
+
+const visit = exports.visit = function visit(fn, classIds) {
+  classIds = classIds || providerIds;
+  console.error(classIds)
+  binding.visit(fn, classIds);
 }
 
-exports.collect = function collect() {
-  var acc = {};
+exports.collect = function collect(classIds) {
+  const acc = {};
   function onpersistent(classid, obj) {
     if (!acc[classid]) acc[classid] = [];
     acc[classid].push(obj);
   }
-  visit(onpersistent);
+  visit(onpersistent, classIds);
   return acc;
 }
--- src/persistents_with_classid.cc
+++ src/persistents_with_classid.cc
@@ -1,6 +1,9 @@
 #include <v8.h>
 #include <node.h>
 
+#include <set>
+
+using v8::Array;
 using v8::Exception;
 using v8::Function;
 using v8::FunctionCallbackInfo;
@@ -13,6 +16,7 @@ using v8::Object;
 using v8::Persistent;
 using v8::PersistentHandleVisitor;
 using v8::String;
+using v8::Uint32;
 using v8::Value;
 using v8::V8;
 
@@ -31,28 +35,41 @@ class PersistentHandleWithClassIdVisitor : public PersistentHandleVisitor {
   public:
     PersistentHandleWithClassIdVisitor(
         Isolate* isolate,
-        Local<Function> fn) {
-      fn_p.Reset(isolate, fn);
+        Local<Function> fn,
+        Local<Array> class_ids) {
+      fn_p_.Reset(isolate, fn);
+
+      // Copy array into a set for faster lookup
+      for (uint32_t i = 0; i < class_ids->Length(); ++i) {
+        Local<Value> el = class_ids->Get(i);
+        if (!el->IsUint32()) {
+          Local<String> message =
+            String::NewFromUtf8(isolate, "All class_ids need to be uints.");
+          isolate->ThrowException(Exception::TypeError(message));
+          return;
+        }
+        class_ids_.insert(static_cast<uint16_t>(el->Uint32Value()));
+      }
     };
 
     ~PersistentHandleWithClassIdVisitor() {
-      fn_p.Reset();
+      fn_p_.Reset();
+      class_ids_.clear();
     }
 
     virtual void VisitPersistentHandle(Persistent<Value>* value,
                                        uint16_t class_id) {
       Isolate* isolate = Isolate::GetCurrent();
       HandleScope handle_scope(isolate);
-      Local<Function> fn = PersistentToLocal(isolate, &fn_p).As<Function>();
-
-      // Don't need to inspect Buffers
-      // (not needed for node >=4.1 since buffers are no longer persistents?)
-      if (class_id == 0xA10C)
-        return;
+      Local<Function> fn = PersistentToLocal(isolate, &fn_p_).As<Function>();
 
       // class_id adjustment for AsyncWrap
       if (class_id >= 0xA1C && class_id < 0xA1C + 100)
         class_id = class_id - 0xA1C;
+
+      // ignore all class ids the user isn't interested in
+      if (class_ids_.find(class_id) == class_ids_.end())
+          return;
 
       Local<Value> obj = PersistentToLocal(isolate, value);
 
@@ -67,19 +84,28 @@ class PersistentHandleWithClassIdVisitor : public PersistentHandleVisitor {
       fn->Call(Null(Isolate::GetCurrent()), 2, argv);
     }
   private:
-    Persistent<Function> fn_p;
+    Persistent<Function> fn_p_;
+    std::set<uint16_t> class_ids_;
 };
 
 static void Visit(const FunctionCallbackInfo<Value>& info) {
   Isolate* isolate = info.GetIsolate();
   if (!info[0]->IsFunction()) {
     Local<String> message =
-      String::NewFromUtf8(isolate, "First arg needs to be a function");
+      String::NewFromUtf8(isolate, "First argument needs to be a Function");
+    isolate->ThrowException(Exception::TypeError(message));
+    return;
+  }
+  if (!info[1]->IsArray()) {
+    Local<String> message =
+      String::NewFromUtf8(isolate, "Second argument needs to be an Array");
     isolate->ThrowException(Exception::TypeError(message));
     return;
   }
 
-  PersistentHandleWithClassIdVisitor visitor(isolate, info[0].As<Function>());
+  PersistentHandleWithClassIdVisitor visitor(isolate,
+                                             info[0].As<Function>(),
+                                             info[1].As<Array>());
   V8::VisitHandlesWithClassIds(isolate, &visitor);
 }
 
