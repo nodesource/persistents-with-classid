4dc2952acf48a9558ec282a5d359b21f0c27efc8
Thorsten Lorenz <thlorenz@gmx.de>
2015-10-04 13:23:26 +0200

    test: tcpwrap

---
+++ test/14_tcpwrap.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const test = require('tape').test
+const net = require('net')
+const persistents = require('../')
+const provider = require('./util/get-provider')('TCPWRAP')
+const PORT = 3000
+
+test('\ntcpwrap: net.createServer', function (t) {
+  const server = net.createServer().listen(PORT);
+
+  const res = persistents.collect()[provider]
+  t.equal(res.length, 1, 'one pipeconnectwrap')
+  t.equal(res[0].owner._connectionKey, '6::::3000', 'the server we created')
+  server.close(t.end)
+})
