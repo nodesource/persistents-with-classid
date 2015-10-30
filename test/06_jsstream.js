9a9295fb1d639d929e269bc14c1b24488062a773
Thorsten Lorenz <thlorenz@gmx.de>
2015-10-30 16:18:07 -0300

    test: fixing JSStreamWrap

--- test/06_jsstream.js
+++ test/06_jsstream.js
@@ -1,15 +1,17 @@
 'use strict';
 
 const test = require('tape').test
-const persistents = require('../../')
+const persistents = require('../')
 const StreamWrap = require('_stream_wrap').StreamWrap;
 const fs = require('fs')
-const provider = require('../util/get-provider')('JSSTREAM')
+const provider = require('./util/get-provider')('JSSTREAM')
 
 test('\njsstream: wrapping a stream via StreamWrap', function (t) {
-  new StreamWrap(fs.createReadStream(__filename))
+  const streamWrap = new StreamWrap(fs.createReadStream(__filename))
   const res = persistents.collect()[provider]
   t.equal(res.length, 1, 'one JSStream')
   t.equal(res[0].owner.stream.path, __filename, 'the wrapped stream')
   t.end()
+  // somehow the stream wrap keeps the process alive, so we force quit it here
+  process.reallyExit()
 })
